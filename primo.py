8425b24de18d38f47df6ffe17c0ec3db8fe14fcc
dariosalascardyn <167145326+dariosalascardyn@users.noreply.github.com>
2024-04-18 20:17:18 -0400

    Create primo.py

---
+++ primo.py
@@ -0,0 +1,14 @@
+
+# numeroprimo = int(input("ingrese un numero entero positivo\n"))
+
+# if numeroprimo % 1 == 0 and numeroprimo % numeroprimo ==0:
+#   print(f"el numero {numeroprimo} es un numero primo")
+# else:  
+#   print("el numero no es primo")
+#   numeroprimo = int(input("ingrese otro numero entero positivo\n"))
+      
+#print(f"el numero {numeroprimo} es un numero primo")
+
+if numeroprimo % 2 == 0 and numeroprimo % 3 == 0:
+  print(f"el numero {numeroprimo} ingresado no es primo")
+ 
